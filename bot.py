ea4f0033c487c7a952010a14a6e0aaae5fab92d7
huynhquangdev <141423816+Quangqq@users.noreply.github.com>
2024-09-29 19:48:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ import concurrent.futures
 
 
 #Telegram token
-token = os.getenv('TELEGRAM_TOKEN')
+token = os.getenv('6791658727:AAHOcnxXSNfXNLAyq21HhL8iK1jg2ggWBSY')
 bot_number = os.getenv('NUM')
 updater = Updater(token,use_context=True)
 s = 360
